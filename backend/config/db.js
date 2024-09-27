83bd02293c52c1d55bec5b488ae7e5968eb89487
@jkkhanh <lekhanhdev.it@gmail.com>
2024-09-27 08:35:41 +0700

    first

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://reactfs:252508@reactfsk.jy2rt.mongodb.net/reactfs'). then(() => console.log("DB connected"))
+    await mongoose.connect(process.env.MONGO_URI).then(() => console.log("DB connected"));
+
 }
